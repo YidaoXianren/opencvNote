96522335404f915559cad95d140e3c5e2b87f518
YidaoXianren <42054519+YidaoXianren@users.noreply.github.com>
2018-09-05 08:39:46 -0400

    Create loadimg.cpp

---
+++ loadimg.cpp
@@ -0,0 +1,18 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+using namespace cv;
+
+int main(int argc, char *argv[])
+{
+    //1 for color image, 0 for grayscale
+    //the last input parameter can also be CV_LOAD_IMAGE_COLOR
+    //or COLOR_LOAD_IMAGE_GRAYSCALE 
+    Mat img = imread("1.jpg", 1);
+    if(img.empty())
+       return -1;
+    namedWindow( "naruto", CV_WINDOW_AUTOSIZE );
+    imshow("naruto", img);
+    waitKey(0);
+    return 0;
+}
